98522e0d06faad4c5cd0303292417710231d98c5
alice <ojamarushealice@yahoo.com>
2020-08-06 20:41:58 -0700

    Progress

--- client/src/client.js
+++ client/src/client.js
@@ -4,17 +4,25 @@ const socket = io("http://127.0.0.1:3400");
 var statusTextEl = document.getElementById("connection-status");
 var userCountEl = document.getElementById("user-count");
 var buttonsContainerEl = document.getElementById("emoji-buttons");
+var emojiPoolEl = document.getElementById("emoji-pool");
+var streamingModeEl = document.getElementById("streaming-mode");
 
 const emojis = [];
-emojis.push({name: "clap", url: require("emoji-images/pngs/clap.png").default});
-emojis.push({name: "heart", url: require("emoji-images/pngs/sparkling_heart.png").default});
-emojis.push({name: "respects", url: require("./images/f_key.png").default});
-emojis.push({name: "airhorn", url: require("./images/airhorn.png").default});
+emojis.push({name: "clap", url: require("emoji-images/pngs/clap.png").default, key: "C", keyCode: 67});
+emojis.push({name: "heart", url: require("emoji-images/pngs/sparkling_heart.png").default, key: "H", keyCode: 72});
+emojis.push({name: "respects", url: require("./images/f_key.png").default, key: "F", keyCode: 70});
+emojis.push({name: "airhorn", url: require("./images/airhorn.png").default, key: "A", keyCode: 65});
 
+var lastId = -1;
 
 socket.on("connect", () => {
   console.log("connected");
   statusTextEl.innerText = "Connected!"
+});
+
+socket.on("disconnect", () => {
+  console.log("disconnected");
+  statusTextEl.innerText = "Disconnected~!"
 });
 
 socket.on("users", (count) => {
@@ -22,24 +30,167 @@ socket.on("users", (count) => {
   userCountEl.innerText = count;
 });
 
-socket.on("emoji", (emote) => {
-  console.log("emoji", emote);
+socket.on("update", (update) => {
+  userCountEl.innerText = update.userCount;
+
+  for (let name in update.emojis) {
+    animate(name, update.emojis[name]);
+  }
 });
 
+socket.on("emoji", (name) => {
+  animate(name, 1);
+});
 
+function findEmojiByName(name) {
+  for (let i = 0; i < emojis.length; i++) {
+    if (emojis[i].name == name) {
+      return emojis[i];
+    }
+  }
+  console.log("couldn't find", name);
+  return null;
+}
 
+const animProps = {
+  col: {max: 19, dupeLength: 10},
+  speed: {max: 8, dupeLength: 4}
+};
+
+let lastProps = {
+  col: [],
+  speed: []
+};
+
+function rand(min, max) {
+  return Math.round(Math.random() * (max - min)) + min;
+}
+
+const POOL_SIZE = 100;
+var openDivs = [];
+
+for (let i = 0; i < POOL_SIZE; i++) {
+  let div = document.createElement("DIV");
+  div.classList.add("emoji");
+  emojiPoolEl.appendChild(div);
+  openDivs.push(div);
+}
+
+function animate(name, count) {
+  let emoji = findEmojiByName(name);
+  if (!emoji) {
+    return;
+  }
+
+  if (openDivs.length === 0) {
+    console.log("no open divs");
+    return;
+  }
+
+  let div = openDivs.shift();
+
+  for (let prop in animProps) {
+    let value = 0;
+    do {
+      value = rand(1, animProps[prop].max);
+    } while (lastProps[prop].indexOf(value) > -1);
+    lastProps[prop].push(value);
+
+    if (lastProps[prop].length > animProps[prop].dupeLength) {
+      lastProps[prop].shift();
+    }
+
+    div.setAttribute("data-" + prop, value);
+  }
+
+  let size = 1;
+  if ( count > 50 ) {
+    size = 4;
+  } else if (count > 25) {
+    size = 3;
+  } else if (count > 10) {
+    size = 2;
+  }
+  div.setAttribute("data-esize", size);
+  div.style.backgroundImage = "url(" + emoji.url + ")";
+
+  // start animation
+  div.setAttribute('data-animation', 1)
+
+  setTimeout(function(){
+    div.removeAttribute("data-animation");
+    openDivs.push(div);
+  }, 7000);
+
+}
+
+var nextEmit = null;
+const USE_BULK = true;
+const DISABLE_SPAM = false;
+var keyCodeToNameMap = {};
+
+function requestEmoji(name, buttonEl) {
+  if (USE_BULK) {
+    if (!nextEmit) {
+      nextEmit = {};
+    }
+    if (!nextEmit[name]) {
+      nextEmit[name] = 1;
+    } else {
+      nextEmit[name]++;
+    }
+
+  } else {
+    socket.emit("emoji-request", name);
+  }
+}
 
 function createButton(emoji) {
   var buttonEl = document.createElement("DIV");
   buttonEl.classList.add("button");
   buttonEl.style.backgroundImage = "url(" + emoji.url + ")";
   buttonEl.addEventListener("mousedown", () => {
-    console.log("requesting", emoji.name);
-    socket.emit("emoji-request", emoji.name);
+    requestEmoji(emoji.name);
+    if (DISABLE_SPAM) {
+      buttonEl.classList.add("disabled");
+      setTimeout(() => {
+        buttonEl.classList.remove("disabled");
+      }, 1000);
+    }
   });
+
+  let keyEl = document.createElement("DIV");
+  keyEl.classList.add("key");
+  keyEl.innerText = emoji.key;
+  buttonEl.appendChild(keyEl);
+  keyCodeToNameMap[emoji.keyCode] = emoji.name;
+
   buttonsContainerEl.appendChild(buttonEl);
 }
+
+if (USE_BULK) {
+  setInterval(function(){
+    if (nextEmit) {
+      console.log("request", nextEmit);
+      socket.emit("emoji-bulk-request", nextEmit);
+      nextEmit = null;
+    }
+  }, 200);
+  }
+
 
 for (var i = 0; i < emojis.length; i++) {
   createButton(emojis[i]);
 }
+
+streamingModeEl.addEventListener("click", function(){
+  document.body.classList.add("streaming");
+});
+
+document.body.addEventListener("keydown", function(e){
+  if (e.keyCode == 81) {
+    document.body.classList.remove("streaming");
+  } else if (keyCodeToNameMap[e.keyCode]) {
+    requestEmoji(keyCodeToNameMap[e.keyCode]);
+  }
+});
--- server/src/index.js
+++ server/src/index.js
@@ -2,33 +2,70 @@ var app = require("express")();
 var http = require("http").createServer(app);
 var io = require('socket.io')(http);
 
-
-
-
 var userCount = 0;
+
+const validEmojis = [
+  "clap",
+  "heart",
+  "respects",
+  "airhorn",
+]
+
+var nextUpdate = {};
+
+function requestBulkEmoji(name, count) {
+  if (validEmojis.indexOf(name) < 0) {
+    return false;
+  }
+  if (!nextUpdate[name]) {
+    nextUpdate[name] = count;
+  } else {
+    nextUpdate[name] += count;
+  }
+ }
+
 
 io.on('connection', (socket) => {
   ++userCount;
-  console.log('User connected, now', userCount, 'connected');
+  console.log('User connected');
 
-  io.emit('users', userCount);
-
+  socket.emit('users', userCount);
 
   socket.on('disconnect', () => {
     --userCount;
-    console.log('User disconnected, now', userCount, 'connected');
-    io.emit('users', userCount);
+    console.log('User disconnected');
   });
 
-  socket.on('emoji-request', (request) => {
+  socket.on('emoji-request', (name) => {
+    if (validEmojis.indexOf(name) < 0) {
+      return false;
+    }
+    io.emit('emoji', name);
+  });
+
+  socket.on('emoji-bulk-request', (request) => {
     console.log("emoji-request", request);
+    for (var name in request) {
+      requestBulkEmoji(name, request[name]);
+    }
   });
-
 });
 
+setInterval(() => {
+  let update = {
+    userCount: userCount,
+    emojis: nextUpdate
+  };
+  io.emit('update', update);
+  console.log(update);
+  nextUpdate = {};
+}, 200);
 
 
-
+// setInterval(() => {
+//   let name = (Math.random() > 0.5) ? "clap" : "heart";
+//   io.emit("emoji", name);
+// }, 200)
 
 http.listen(3400, () => {
   console.log("Started");
